4ccbb0d0f8727f90cbeba75217c5095aaf277d30
Aldebalan <tjdgus4093@naver.com>
2022-06-09 11:20:36 +0900

    logex #fin

--- logex/src/main/java/com/douzone/logex1/controller/ExampleController.java
+++ logex/src/main/java/com/douzone/logex1/controller/ExampleController.java
@@ -21,9 +21,8 @@ public class ExampleController {
 		/**
 		 * 이 예제에서는 logback.xml 에서,
 		 * 
-		 * 1. consoleAppender 와 root logger 만 세팅해서 테스트 해 볼 수 있습니다. 2. root logger의
-		 * level="DEBUG" 조정해 봅니다.
-		 * 
+		 * 1. consoleAppender 와 root logger 만 세팅해서 테스트 해 볼 수 있습니다.
+		 * 2. root logger의 level="DEBUG" 조정해 봅니다.
 		 * 3. 테스트 해보면, DEBUG > INFO > WARN > ERROR 순으로 로그가 출력 되는 것을 확인할 수 있습니다.
 		 * 
 		 * 가령, WARN 설정하면, warn, error 메서드의 로그 메세지만 출력 됩니다. 직접 테스트 해 보세요.
